4eb6e3b67ab69cdc691120b8de41d77cf8f186e5
Adel Senhadji <adelsenhadjii@gmail.com>
2020-04-04 14:58:41 +0200

    init server-side project

---
+++ wt-back/src/main/java/com/adxl/weigthtracker/WeigthTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.adxl.weigthtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeigthTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeigthTrackerApplication.class,args);
+	}
+}
---
+++ wt-back/src/test/java/com/adxl/weigthtracker/WeigthTrackerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.adxl.weigthtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeigthTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
